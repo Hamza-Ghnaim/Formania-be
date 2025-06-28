b90f60374d7bca6e7a18ada7747c8b59ca72eaf0
Hamza <hamzaghnaimm99@gmail.com>
2025-06-28 15:32:59 +0300

    feat: add sequelize

--- config/db.ts
+++ config/db.ts
@@ -1,11 +1,16 @@
 // config/db.ts
-import mysql from "mysql2/promise"; // Use promises for async/await
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-export const pool = mysql.createPool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DATABASE as string,
+  process.env.DATABASE_USER as string,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+    logging: false, // set to true for SQL logs
+  }
+);
--- routes/login.ts
+++ routes/login.ts
@@ -1,32 +1,14 @@
 import express from "express";
-import { Request, Response } from "express";
-
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { Pool } from "mysql2/promise";
+import { User } from "../models/User.js";
 
 dotenv.config(); // Ensure this line is at the top, before accessing process.env
-
-interface User {
-  id: number;
-  email: string;
-  username: string;
-  password_hash: string;
-}
 
 interface LoginRequest {
   email: string;
   password: string;
-}
-
-interface LoginResponse {
-  message: string;
-  user: {
-    id: number;
-    username: string;
-    email: string;
-  };
 }
 
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -36,57 +18,51 @@ if (!SECRET_KEY) {
 
 const router = express.Router();
 
-export default (pool: Pool) => {
-  router.post(
-    "/login",
-    async (req: express.Request<{}, {}, LoginRequest>, res: any) => {
-      const { email, password } = req.body;
+router.post(
+  "/login",
+  async (req: express.Request<{}, {}, LoginRequest>, res: any) => {
+    const { email, password } = req.body;
 
-      if (!email || !password) {
-        return res
-          .status(400)
-          .json({ message: " Email and Password are required" });
-      }
-
-      try {
-        const query = "Select * from users where email =?";
-        const [rows] = await pool.query(query, [email]);
-        const results = rows as User[];
-        if (results.length === 0) {
-          return res.status(400).json({ message: "User not Found" });
-        }
-        const user = results[0];
-
-        const isPasswordValid = await bcrypt.compare(
-          password,
-          user.password_hash
-        );
-        if (!isPasswordValid) {
-          return res.status(401).json({ message: "Invalid Password" });
-        }
-
-        const token = jwt.sign(
-          { userId: user.id, email: user.email },
-          SECRET_KEY,
-          { expiresIn: "1h" }
-        );
-
-        res.cookie("token", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-          maxAge: 3600000,
-        });
-
-        return res.status(200).json({
-          message: "Login Successfully",
-          user: { id: user.id, email: user.email, username: user.username },
-        });
-      } catch (error) {
-        console.error("Error during login:", error);
-        res.status(500).json({ message: "An error occurred during login." });
-      }
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: " Email and Password are required" });
     }
-  );
-  return router;
-};
+
+    try {
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        return res.status(400).json({ message: "User not found" });
+      }
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        user.password_hash
+      );
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid Password" });
+      }
+
+      const token = jwt.sign(
+        { userId: user.id, email: user.email },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 3600000,
+      });
+
+      return res.status(200).json({
+        message: "Login Successfully",
+        user: { id: user.id, email: user.email, username: user.username },
+      });
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(500).json({ message: "An error occurred during login." });
+    }
+  }
+);
+export default router;
